3e2316406ca78beeb2acd7c29ce63ad5955b78ce
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-10-16 18:04:16 +0300

    created a bunch of files

--- src/component/SearchMovies.jsx
+++ src/component/SearchMovies.jsx
@@ -1,23 +1,30 @@
-import {useState, useEffect} from "react"
+import {useState, useEffect, useContext} from "react"
+import { MovieStore } from "../store/MovieStore";
 
-export default function LetsFetch() {
+export default function SearchMovies() {
     const [keyID, setKeyID] = useState('')
-    const [data, setData] = useState([])
+    const { setMovies, movies } = useContext(MovieStore);
     const API_KEY = process.env.REACT_APP_API_KEY
     const url = `http://www.omdbapi.com/?s=${keyID}&apikey=${API_KEY}`
 
     useEffect(()=>{
         fetch(url)
         .then((resp) => resp.json())
-        .then((movies) => setData(movies))
-    }, [url])
+        .then((movies) => 
+        setMovies(movies))
+    }, [url, setMovies])
 
     return (
         <div className="searchBar">
         <h3>Seach Movie Title</h3>
-        <form onSubmit={(e)=> e.preventDefault()}>
-        <input placeholder="Movie Title" onChange={(e) => setKeyID(e.target.value)}></input>
-        <button onClick={()=> console.log(data)}>Search</button>
+        <form 
+        onSubmit={(e)=> e.preventDefault()}>
+        <input 
+        placeholder="Movie Title" 
+        value={keyID}
+        onChange={(e) => setKeyID(e.target.value)}>
+        </input>
+        <button type="submit" onClick={()=>console.log(movies)}>Search</button>
         </form>
         </div>
     )
